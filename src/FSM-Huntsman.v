f907f35b37b27456301abb6e93442bc474e7576a
bhunt2 <bhunt2@pdx.edu>
2013-12-02 18:40:54 -0800

    good stuff

--- src/FSM-Huntsman.v
+++ src/FSM-Huntsman.v
@@ -4,17 +4,15 @@
  *  by PowerPC to do the same.
  */
 
-module cache_FSM(CLK, RST, COMMAND, FSB, L1BUS, HM, STATE);
+module cache_FSM(COMMAND, STATE_IN, HM, STATE_OUT);
+  
   // Declare input/output types, sizes, and uses
-  input         CLK;                      // Clock to run the FSM
-  input [31:0]  COMMAND;                  // Command is an integer giving the current operation being pushed to L2
-  inout [31:0]  L1BUS;                    // Bus between Data/Instruction Caches and L2 Cache
-  inout [63:0]  FSB;                      // Shared bus between others processors and memory
-  inout [1:0]   HM;                       // Takes the HIT/HITM/MISS signals from FSB: 0 MISS, 1 HIT, 2 HITM
-  inout [3:0]   STATE;                    // The current state of the working address
-
+  input  [7:0]   COMMAND;                 // The command of the currently working operation
+  input  [3:0]    STATE_IN;               // The current state of the working address
+  input  [1:0]    HM;                     // Takes the HIT/HITM/MISS signals from FSB: 0 MISS, 1 HIT, 2 HITM
+  output [3:0]    STATE_OUT;              // The next state of the address according to other inputs and MESI FSM
+  
   // Establish needed variables and parameters
-  reg           next_state;               // Used for state transitioning
   localparam    MODIFIED    = 4'b0001;    // Params used for MESI protocol
   localparam    EXCLUSIVE   = 4'b0010;
   localparam    SHARED      = 4'b0100;
@@ -27,62 +25,49 @@ module cache_FSM(CLK, RST, COMMAND, FSB, L1BUS, HM, STATE);
   /*   the next state.                                                                                      */
   /**********************************************************************************************************/
 
-  // Performs state transition
-  always @(posedge CLK or negedge RST)
-  begin
-    if(!RST)
-      // Call clear and reset function
-    else
-      STATE = next_state;
-  end
 
   // Gives next state
   always @(*)
   begin
-    case(current_state)
-      MODIFIED:   if      (COMMAND == 0)  begin PASS2L1('L1DR'); next_state = MODIFIED; end
-                  else if (COMMAND == 1)  begin WRITE2L2; next_state = MODIFIED; end
-                  else if (COMMAND == 2)  next_state = MODIFIED;  // not sure because should not happen?
-                  else if (COMMAND == 3)  next_state = MODIFIED;  // not sure because should not happen?
-                  else if (COMMAND == 4)  next_state = SHARED;
-                  else if (COMMAND == 5)  next_state = MODIFIED;  // not sure because should not happen?
-                  else if (COMMAND == 6)  next_state = INVALID;
+    case(STATE_IN)
+      MODIFIED:   if      (COMMAND == 0) STATE_OUT = MODIFIED;
+                  else if (COMMAND == 1) STATE_OUT = MODIFIED;
+                  else if (COMMAND == 2) STATE_OUT = MODIFIED;  // not sure because should not happen?
+                  else if (COMMAND == 3) STATE_OUT = MODIFIED;  // not sure because should not happen?
+                  else if (COMMAND == 4) STATE_OUT = SHARED;
+                  else if (COMMAND == 5) STATE_OUT = MODIFIED;  // not sure because should not happen?
+                  else if (COMMAND == 6) STATE_OUT = INVALID;
 
-      EXCLUSIVE:  if      (COMMAND == 0)  begin PASS2L1('L1DR'); next_state = EXCLUSIVE; end
-                  else if (COMMAND == 1)  begin INVALIDATESHARED; WRITE2L2; next_state = MODIFIED; end
-                  else if (COMMAND == 2)  begin PASS2L1('L1IR'); next_state = EXCLUSIVE; end
-                  else if (COMMAND == 3)  next_state = INVALID;
-                  else if (COMMAND == 4)  next_state = SHARED;
-                  else if (COMMAND == 5)  next_state = EXCLUSIVE;  // not sure because should not happen?
-                  else if (COMMAND == 6)  next_state = INVALID;
+      EXCLUSIVE:  if      (COMMAND == 0) STATE_OUT = EXCLUSIVE;
+                  else if (COMMAND == 1) STATE_OUT = MODIFIED;
+                  else if (COMMAND == 2) STATE_OUT = EXCLUSIVE;
+                  else if (COMMAND == 3) STATE_OUT = INVALID;
+                  else if (COMMAND == 4) STATE_OUT = SHARED;
+                  else if (COMMAND == 5) STATE_OUT = EXCLUSIVE;  // not sure because should not happen?
+                  else if (COMMAND == 6) STATE_OUT = INVALID;
 
-      SHARED:     if      (COMMAND == 0) begin
-                    PASS2L1('L1DR'); // Push data onto L1BUS - this needs to be replaced with the appropriate operation
-                    next_state = SHARED;
+      SHARED:     if      (COMMAND == 0) STATE_OUT = SHARED;
+                  else if (COMMAND == 1) STATE_OUT = MODIFIED;
+                  else if (COMMAND == 2) STATE_OUT = SHARED;
+                  else if (COMMAND == 3) STATE_OUT = SHARED;  // not sure because should not happen?
+                  else if (COMMAND == 4) STATE_OUT = SHARED;
+                  else if (COMMAND == 5) STATE_OUT = SHARED;  // not sure because should not happen?
+                  else if (COMMAND == 6) STATE_OUT = INVALID;
+
+      INVALID:    if      (COMMAND == 0) begin
+                    if    (HM >= 2'b1)   STATE_OUT = SHARED;
+                    else if (HM = 2'b0)  STATE_OUT = EXCLUSIVE;
+                    else                 STATE_OUT = INVALID;
                   end
-                  else if (COMMAND == 1) begin INVALIDATESHARED; WRITE2L2; next_state = MODIFIED; end
-                  else if (COMMAND == 2) begin PASS2L1('L1IR'); next_state = SHARED; end
-                  else if (COMMAND == 3)  next_state = SHARED;  // not sure because should not happen?
-                  else if (COMMAND == 4)  next_state = SHARED;
-                  else if (COMMAND == 5)  next_state = SHARED;  // not sure because should not happen?
-                  else if (COMMAND == 6)  next_state = INVALID;
-
-      INVALID:    if      (COMMAND == 0) begin // this procedures waits until an answer is recieved from the fsb before moving on.
-                    FSBREAD(); // Push read request on shared bus to get requested data.
-                    if    (HM >= 2'b1)    next_state = SHARED;
-                    else if (HM = 2'b0)   next_state = EXCLUSIVE;
-                    else                  next_state = INVALID;
-                  end
-                  else if (COMMAND == 1) begin next_state = MODIFIED; end // This needs more thought
+                  else if (COMMAND == 1) STATE_OUT = MODIFIED; // This needs more thought
                   else if (COMMAND == 2) begin
-                    FSBREAD(); // Push read request on shared bus
-                    if    (HM >= 2b'1)    next_state = SHARED;
-                    else if (HM = 2'b0)   next_state = EXCLUSIVE;
-                    else                  next_state = INVALID;
-                  else if (COMMAND == 3)  next_state = INVALID;  // not sure because should not happen?
-                  else if (COMMAND == 4)  next_state = SHARED;
-                  else if (COMMAND == 5)  next_state = INVALID;  // not sure because should not happen?
-                  else if (COMMAND == 6)  next_state = INVALID;
+                    if    (HM >= 2b'1)   STATE_OUT = SHARED;
+                    else if (HM = 2'b0)  STATE_OUT = EXCLUSIVE;
+                    else                 STATE_OUT = INVALID;
+                  else if (COMMAND == 3) STATE_OUT = INVALID;  // not sure because should not happen?
+                  else if (COMMAND == 4) STATE_OUT = SHARED;
+                  else if (COMMAND == 5) STATE_OUT = INVALID;  // not sure because should not happen?
+                  else if (COMMAND == 6) STATE_OUT = INVALID;
     endcase
   end
 endmodule
