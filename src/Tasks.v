a6020768d02f235007303cb8b247ec8c1060a2db
Michael O Walton <waltonm@ruby.cat.pdx.edu>
2013-12-05 15:24:51 -0800

    Merged L2Cache and pulled tasks back out in to Tasks.v

--- src/Tasks.v
+++ src/Tasks.v
@@ -41,3 +41,49 @@ endtask
 // Print contents and state of each valid
 task command9 ();
 endtask
+
+task QueryLRU (input index[indexBits - 1:0],
+               output way[$clog2(ways) - 1:0]);
+begin
+  // Loop through the set array at row[index]
+  // Store LRU bits for each way
+  //for (i=0,i<ways,++i)
+  //for (j=0,j<$clog2(ways),++j)
+  //LRUbits[j][i]=set[i][index].LRUbits[j][i];
+
+  integer LRUvalue = 0;
+  integer i;
+
+  // Check to see which is the least recently used
+  // and set the appropriate way
+  for (i = 0,i < ways, i = i + 1)
+    if (LRUbits[i][$clog2(ways)-1:0] > LRUvalue)
+    begin
+      LRU value = LRUbits[i][$clog2(ways)-1:0];
+      way = i;
+    end
+  end
+endtask
+
+task UpdateLRU  (input index[indexBits - 1:0], way[$clog2(ways) - 1:0]);
+  begin
+    // Array to store the LRU bits from each way
+    reg LRUbits[$clog2(ways) - 1:0][$clog2(ways) - 1:0];
+
+    // Loop through the set array at row[index]
+    integer i, j;
+    for (i = 0; i < ways; i = i + 1)
+      for (j = 0; j < $clog2(ways); j = j  +1)
+        LRUbits[j][i]=set[i][index].lru[j][i];
+
+      // Store LRU bits for each way
+      // Run LRU logic to update fields given
+      // Initialise all the bits to '0' first
+      // The accessed line is set to '0' (in later cases it is brought down to '0' but here its already 0 and remains same )
+      //After that increment all the bits assigned to other lines by 1
+      // In this case take one more condition like if the line already has a higher bit than any other bit , then it remains same
+        // So, if it is already highest , keep it the same
+          // If more than one line has same bit assigned to it, then evict the line that comes first from left
+          // which way is being read/written to
+        end
+      endtask
