b8f60d12df5cea326834cc9d30c850d90cc9c90d
bhunt2 <bhunt2@pdx.edu>
2013-12-05 21:52:10 -0800

    create getsnoopresult module and updated toplevel module

---
+++ src/GetSnoopResult.sv
@@ -0,0 +1,18 @@
+module GetSnoopResult(address,operation,snoopBus);
+  // Configurable params for dynamic system
+  parameter addressSize = 32;
+  
+  // Establish inputs and outputs of the module
+  input [addressSize - 1:0] address;
+  input [7:0]               operation;
+  inout [1:0]               snoopBus;
+  
+  // Perform a consistant procedure
+  always @(address,operation) begin
+    if(address[3:0] == 4'h2 || address [3:0] == 4'h8)
+      snoopBus = 2'b01;
+    else if(address[3:0] == 4'h4 || address[3:0] == 4'hC)
+      snoopBus = 2'b10;
+    else
+      snoopBus = 2'b00;
+  end
